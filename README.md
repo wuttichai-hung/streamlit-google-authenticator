49ae81617f383234e3bd3b12156b69330ae28c1e
wuttichaihung <wuttichaihung@gmail.com>
2023-03-09 03:12:09 +0700

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Streamlit - Google Authenticator
+This project was created by [datahungry.dev](https://www.facebook.com/datahungry)
+
+## Pre-requirement
+```bash
+pip install -r requirements.txt
+```
+
+## Let's run it
+```bash
+streamlit run streamlit_app.py
+```
---
+++ streamlit_app.py
@@ -0,0 +1,30 @@
+import re
+import pyotp
+import qrcode
+import streamlit as st
+
+st.set_page_config(
+    page_title="DataHungry - Google Authenticator",
+    page_icon=":bee:",
+    layout="centered"
+)
+
+if "secret" not in st.session_state:
+    st.session_state.secret = pyotp.random_base32()
+
+totp = pyotp.totp.TOTP(st.session_state.secret).provisioning_uri(
+    name="Demo GA", issuer_name="DataHungry")
+qr_byte_data = qrcode.make(totp).get_image()
+
+
+st.markdown("# Google Authenticator")
+st.image(qr_byte_data, caption="QR Code - Google Authenticator")
+
+
+code = st.text_input("Enter the Code : ")
+if st.button("Verify"):
+    totp = pyotp.TOTP(st.session_state.secret)
+    if totp.verify(re.sub(r"\s", "", code)):
+        st.success("Code verified successfully!", icon="✅")
+    else:
+        st.error("Sorry, the code could not be verified. Please try again.", icon="🚨")
